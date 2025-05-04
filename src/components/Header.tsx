6f8a738c4d081abd0b57762191dcfae29e9bd0ce
charang394 <charang394@gmail.com>
2025-05-04 16:34:37 +0000

    signout and user component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,6 +20,78 @@ const Header = () => {
           </SearchIcon>
          
         </Search>
+        <Nav>
+          <NavWrapper>
+              <NavItem className="active">
+                <a href="/Home">
+                  <img src="/nav-home.svg" alt="" />
+                  <span>Home</span>
+                </a>
+              </NavItem>
+             
+              
+              <NavItem>
+                <a href="/Home">
+                  <img src="/nav-network.svg" alt="" />
+                  <span>My Network</span>
+                </a>
+              </NavItem>
+
+              <NavItem>
+                <a href="/Home">
+                  <img src="/nav-jobs.svg" alt="" />
+                  <span>Jobs</span>
+                </a>
+              </NavItem>
+
+              <NavItem>
+                <a href="/Home">
+                  <img src="/nav-messaging.svg" alt="" />
+                  <span>Messaging</span>
+                </a>
+              </NavItem>
+
+              <NavItem>
+                <a href="/Home">
+                  <img src="/nav-notifications.svg" alt="" />
+                  <span>Notifications</span>
+                </a>
+              </NavItem>
+              <User>
+                <a href="/Home">
+                  <img src="/user.svg" alt="" />
+                  <span>Me
+                  <img  src="/down-icon.svg" alt="" />
+                  </span>
+                  
+                </a>
+                <SignOut>
+                  <a href="/">
+                    Sign Out
+                  </a>
+                </SignOut>
+              </User> 
+
+               <Work>
+                <a href="/Home">
+                  <img src="/nav-work.svg" alt="" />
+                  <span>Work
+                  <img src="/down-icon.svg" alt="" />
+                  </span>
+                  
+                </a>
+              </Work> 
+              
+          
+
+          </NavWrapper>
+
+
+
+        </Nav>
+
+
+
       </Content>
     </Container>
   )
@@ -93,6 +165,135 @@ position:absolute;
   justify-content:center;
   align-items:center;
   margin:0;
+`
+  const Nav=styled.div`
+  margin-left:auto;
+  display:block;
+  @media(max-width:768px){
+    position:fixed;
+    left:0;
+    bottom:0;
+    background:white;
+    width:100%;
+  }
+    
+  `
+const NavWrapper=styled.ul`
+    display:flex;
+    flex-wrap:nowrap;
+    list-style-type:none;
+    justify-content:space-around;
+
+    .active a{
+      
+        &::after{
+          content:"";
+          /* transform:scaleX(); */
+          border-bottom:2px solid black;
+          bottom:0;
+          left:0;
+          position:absolute;
+          width:100%;
+          
+      }
+  }
+            
+`
+const NavItem=styled.li`
+display:flex;
+align-items:center;
+
+
+
+
+a{
+
+  align-items:center;
+  background:transparent;
+  display:flex;
+  flex-direction:column;
+  font-size:12px;
+  font-weight:400;
+  justify-content:center;
+  line-height:1.5;
+  min-height:42px;
+  min-width:80px;
+  position:relative;
+  text-decoration:none;
+  span{
+    color:rgb(0,0,0,0.6);
+    display:flex;
+
+    align-items:center;
+  }
+  @media(max-width:768px){
+    min-width:70px;
+  }
+}
+
+  &:hover,&:active{
+    a{
+      span{
+        color:rgb(0,0,0,0.9);
+      }
+    
+  }
+}
+`
+const SignOut=styled.div`
+position:absolute;
+top:42px;
+background:white;
+border-radius:0 0 5px 5px;
+width:100px;
+height:40px;
+font-size:16px;
+transition-duration:167ms;
+text-align:center;
+display:none;
+`
+
+
+
+const User=styled(NavItem)`
+  a{
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+
+  }
+
+a > img{
+  width:24px;
+  border-radius:50%;
+}
+span{
+  display:flex;
+  align-items:center;
+
+
+  img{
+    width:16px;
+    margin-right:5px;
+  
+  }
+}
+
+ &:hover{ ${SignOut}{
+    display:flex;
+    align-items:start;
+    justify-content:center;
+    border-radius:0 0 5px 5px;
+  
+
+  }
+ }
+
+
+`
+const Work=styled(User)`
+border-left: 1px solid rgba(0,0,0,0.8);
 `
 
 
